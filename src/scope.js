9eee259b5e95f643a72e65b4f64725dcead10e04
Yoni <Yoni@hangwiz.com>
2016-01-08 19:35:20 +0200

    a

--- src/scope.js
+++ src/scope.js
@@ -7,21 +7,24 @@ function Scope() {
 Scope.prototype.$watch =function(watchFn, listenFn ) {
     var watcher = { //create the watcher object in the scope watch function and add it to the watchers
         watchFn : watchFn,
-        listenFn :listenFn
+        listenFn :listenFn,
+        last: initWatchVal
+
     } // now add it the watchers array
     this.$$watchers.push(watcher);
     //test
 }
-Scope.prototype.$digest = function () {
+Scope.prototype.$digest = function() {
     var self = this;
     var newValue, oldValue;
-
     _.forEach(this.$$watchers, function(watcher) {
-
-        newValue = watcher.watchFn(self);  // give the watch function it context
-        watcher.listenerFn(newValue, oldValue, self);
-        // evaluate the watch function and see what changes
-
-
-    })
-}
+        newValue = watcher.watchFn(self);
+        oldValue = watcher.last;
+        if (newValue !== oldValue) {
+            watcher.last = newValue;
+            watcher.listenerFn(newValue,
+                (oldValue === initWatchVal ? newValue : oldValue),
+                self);
+        }
+    });
+};
