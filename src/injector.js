a3dda1e73835a23a82b7b67e4955dfc22e630b1f
yoni <yamishav@cisco.com>
2016-03-16 20:28:54 +0200

    update 414

--- src/injector.js
+++ src/injector.js
@@ -5,29 +5,69 @@
 
 
 function createInjector(modulesToLoad) {
-    var cache = {};
+    var cache = {};  // save the modules
+    // we need to deal with circular dependencies to make sure each module is loaded once
+    //
+    var loadedModules = {};
+
     var $provide = {
         constant: function (key, value) {
-            if (key === hasOwnProperty) {
+            if (key === 'hasOwnProperty') {
                 throw  'hasOwnProperty is not a valid constant name!';
             }
             cache[key] = value;
         }
     };
-    _.forEach(modulesToLoad, function (moduleName) {
-        var module = angular.module(moduleName);
-        _.forEach(nodule._invokeQueue, function (invokeArgs) {
-            var method = invokeArgs[0];
-            var args = invokeArgs[1];
-            $provide[method].apply($provide, args);
+    function  anotate(fn) {
+        if(_.isArray(fn)) { // if fn is an array annotate should retunr an array of all but the
+            //last item of it
+            return fn.slice(0,fn.length-1);
+        } else if(fn.$inject) {
+            return fn.$inject;
+        }
+        else
+            return [];
+
+    }
+    function invoke(fn, self,locals) {
+        //For each argument replace it with the cache object
+        var args = _.map(fn.$inject, function (token) {
+            if (_.isString(token))
+            // if the locals can replace the token replace it with the requested value
+                return locals && locals.hasOwnProperty(token) ?
+                    locals[token] :
+                    cache[token];
+            else
+                throw    'Incorrect injection token! Expected a string, got' + token;
         });
+        //after replace run
+        return fn.apply(self, args);
+    }
+
+
+    _.forEach(modulesToLoad, function loadModule(moduleName) {
+        if (!loadedModules.hasOwnProperty(moduleName)) {
+            loadedModules[moduleName] = true;
+            var module = angular.module(moduleName); // ask for the module
+            _.forEach(module.requires, loadModule);
+
+            forEach(nodule._invokeQueue, function (invokeArgs) {
+                var method = invokeArgs[0];
+                var args = invokeArgs[1];
+                $provide[method].apply($provide, args);
+            });
+        }
+
+
     });
     return {
         has: function (key) {
             return cache.hasOwnProperty(key);
         },
-        get: function (key) {
+        get: function (key) { // using clouser
             return cache[key];
-        }
+        },
+        annotate: anotate,
+        invoke: invoke
     };
 }
--- test/injector_spec.js
+++ test/injector_spec.js
@@ -65,4 +65,81 @@ describe('injector', function () {
         expect(injector.has('anotherConstant')).toBe(true);
         expect(injector.has('aThirdConstant')).toBe(true);
     });
+    it('invokes an annotated function with dependency injection', function () {
+        var module = angular.module('myModule', []);
+        module.constant('a', 1);
+        module.constant('b', 2);
+        var injector = createInjector(['myModule']);
+        var fn = function (one, two) {
+            return one + two;
+        };
+        fn.$inject = ['a', 'b'];  // inject a and b
+        expect(injector.invoke(fn)).toBe(3);
+    });
+    it('does not accept non-strings as injection tokens ', function () {
+        var module = angular.module('myModule', []);
+        module.constant('a', 1);
+        var injector = createInjector(['myModule']);
+        var fn = function (one, two) {
+            return one + two;
+        };
+        fn.$inject = ['a', 2];
+        expect(function () {
+            injector.invoke(fn);
+        }).toThrow();
+    });
+    it('invokes a function with the given this context', function () {
+
+        var module = angular.module('myModule', []);
+        module.constant('a', 1);
+        var injector = createInjector(['myModule']);
+        var obj = {
+            two: 2,
+            fn: function (one) {
+                return one + this.two;
+            }
+        };
+        obj.fn.$inject = ['a'];
+        expect(injector.invoke(obj.fn, obj)).toBe(3);
+    });
+    it('overrides dependencies with locals when invoking', function () {
+        var module = angular.module('myModule', []);
+        module.constant('a', 1);
+        module.constant('b', 2);
+        var injector = createInjector(['myModule']);
+        var fn = function (one, two) {
+            return one + two;
+        };
+        fn.$inject = ['a', 'b'];
+        expect(injector.invoke(fn, undefined, {b: 3})).toBe(4);
+    });
+
+
+});
+describe('annotate', function () {
+    it('returns the $inject annotation of a function when it has one', function () {
+        var injector = createInjector([]);
+        var fn = function () {
+        };
+        fn.$inject = ['a', 'b'];
+        expect(injector.annotate(fn)).toEqual([a, b]);
+    });
+    it('returns the array-style annotations of a function', function () {
+        var injector = createInjector([]);
+        var fn = ['a', 'b', function () {
+        }];
+        expect(injector.annotate(fn)).toEqual(['a', 'b']);
+    });
+    it('returns an empty array for a non-annotated 0-arg function', function () {
+        var injector = createInjector([]);
+        var fn = function () {
+        };
+        expect(injector.annotate(fn)).toEqual([]);
+    });
+    it('returns annotations parsed from function args when not annotated', function () {
+        var injector = createInjector([]);
+        var fn = function (a, b) {
+        };
+        expect(injector.annotate(fn)).toEqual(['a', 'b']);
+    });
 });
